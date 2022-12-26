c204a66c047d3bf42543e5f7e8902f7ea3376564
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2022-12-26 11:55:44 +0530

    Create reverseStarTriangle.cpp
    The code reads an integer t from the input, which represents the number of test cases.
    For each test case, the code reads an integer n from the input, which represents the number of rows in the triangle.
    The code calls a function printTriangle which takes an integer as input and prints a right triangle of asterisks (*) with n rows. The first row has one asterisk, the second row has two asterisks, and so on.
    The printTriangle function checks if the input is within the valid range of 1 to 20. If it is, the function prints the triangle by iterating through the rows in decreasing order and printing the asterisks in each row.
    The main function returns 0 to indicate the program has completed successfully

---
+++ reverseStarTriangle.cpp
@@ -0,0 +1,49 @@
+// This is the main driver code which reads the input, calls the Solution class, and prints the output
+#include <bits/stdc++.h> // This is a header file which includes all the standard library headers in one place
+
+using namespace std; // This line allows us to use the standard library functions without using the "std::" prefix
+
+// The Solution class contains the function to print the triangle
+class Solution{
+public:
+	
+	// This function takes an integer n as input and prints a right triangle of asterisks (*) with n rows
+	// The first row has one asterisk, the second row has two asterisks, and so on
+	void printTriangle(int n) {
+	    // The function checks if the input is within the valid range of 1 to 20
+	    if(n>=1 && n<=20){
+	        // The outer loop iterates from n to 1 (in decreasing order)
+	        for(int i=n; i>=1; i--){
+	            // The inner loop iterates from 0 to i (in increasing order)
+	            for(int j=0; j<i; j++){
+	                // This line prints an asterisk and a space for each iteration of the inner loop
+	                cout<<"*"<<" ";
+	            }
+	            // This line prints a newline character after each iteration of the outer loop
+	            cout<<"\n";
+	        }
+	    }
+	}
+};
+
+// This is the main function which runs the program
+int main() {
+    // This variable holds the number of test cases
+    int t;
+    // This line reads the value of t from the input
+    cin >> t;
+    // This loop iterates t times
+    while (t--) {
+        // This variable holds the number of rows for each test case
+        int n;
+        // This line reads the value of n from the input
+        cin >> n;
+        
+        // This line creates an object of the Solution class
+        Solution ob;
+        // This line calls the printTriangle function of the Solution object with the input n
+        ob.printTriangle(n);
+    }
+    // This line returns 0 to indicate the program has completed successfully
+    return 0;
+}
