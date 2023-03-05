a78f1b8b22296ba3ccdfd2f1f5938956438dabb8
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2023-03-05 07:43:41 +0530

    Update and rename samenumberTriangle.cpp to 4. samenumberTriangle.cpp

--- 1. Patterns/4. samenumberTriangle.cpp
+++ 1. Patterns/4. samenumberTriangle.cpp
@@ -1,3 +1,16 @@
+/*
+https://practice.geeksforgeeks.org/problems/triangle-number-1661428795/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_4
+Input: 5
+
+Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+*/
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
