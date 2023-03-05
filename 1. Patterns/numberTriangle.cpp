53f8a33f5f40b5fda7f2b82e168b6fcd5684154c
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2023-03-05 07:40:07 +0530

    Update numberTriangle.cpp

--- 1. Patterns/numberTriangle.cpp
+++ 1. Patterns/numberTriangle.cpp
@@ -1,3 +1,16 @@
+/*
+https://practice.geeksforgeeks.org/problems/triangle-number/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_3
+
+Input: 5
+
+Output:
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
