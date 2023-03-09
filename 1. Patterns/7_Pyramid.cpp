72054c067fdddff526b6a638aa8af35d35e00d7f
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2023-03-09 21:57:28 +0530

    Update 7_Pyramid.cpp

--- 1. Patterns/7_Pyramid.cpp
+++ 1. Patterns/7_Pyramid.cpp
@@ -1,3 +1,21 @@
+/*
+https://practice.geeksforgeeks.org/problems/triangle-pattern-1661492263/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_7
+Input: 5
+Output:
+    *
+   ***  
+  *****
+ *******
+*********
+Algo:
+  1. Start by defining a function called `printTriangle` that takes an integer parameter `N`.
+  2. For each row in the range of 0 to N (inclusive):
+      a. Calculate the number of stars to print in the current row using the formula (2*row)-1, and store it in a variable called `formula`.
+      b. Print the required number of spaces before printing the stars by looping through the range of (N - row) and printing a space for each iteration.
+      c. Print the stars for the current row by looping through the range of `formula` and printing a `*` for each iteration.
+      d. Move to the next line for the next row by printing a newline character.
+  3. End the function.
+  */
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
