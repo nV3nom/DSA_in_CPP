964b8267e8c3adaccc5bd1cb533e6120a7273aee
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2023-03-10 18:32:31 +0530

    Update 8_reversePyramid.cpp

--- 1. Patterns/8_reversePyramid.cpp
+++ 1. Patterns/8_reversePyramid.cpp
@@ -1,3 +1,23 @@
+/*
+https://practice.geeksforgeeks.org/problems/triangle-pattern-1661493231/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_8
+Input: 5
+
+Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+Algo:    
+1. Define a class called Solution.
+2. Inside the class, define a method called printTriangle that takes an integer N as input.
+3. For each row in the triangle, starting from N and going down to 1, do the following:
+   1. For each space in the row (there are N - row spaces), print a space character.
+   2. For each column in the row (there are 2*row - 1 columns), print a star character.
+   3. After all the spaces and stars have been printed for the current row, print a newline character to move to the next row.
+4. Once all rows have been printed, the method returns.
+*/
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
