c589c7022f8950bba0fd46dc1da5ae5eb100c523
Amit Kumar Maity <96053591+nV3nom@users.noreply.github.com>
2023-03-11 15:26:11 +0530

    Update 9_diamond.cpp

--- 1. Patterns/9_diamond.cpp
+++ 1. Patterns/9_diamond.cpp
@@ -1,3 +1,34 @@
+/*
+https://practice.geeksforgeeks.org/problems/pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_9
+For Input: 
+5
+Your Output: 
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+    
+Algo:
+1. Define a class called Solution.
+2. Inside the class, define a method called printDiamond that takes an integer N as input.
+3. For each row in the top half of the diamond (from 1 to N), do the following:
+   1. Calculate the number of spaces needed for this row (N - row).
+   2. Use string multiplication to create a string of spaces of the appropriate length.
+   3. Use string multiplication to create a string of stars for this row (row stars).
+   4. Print the spaces and stars for this row.
+4. For each row in the bottom half of the diamond (from N-1 to 1), do the following:
+   1. Calculate the number of spaces needed for this row (N - row).
+   2. Use string multiplication to create a string of spaces of the appropriate length.
+   3. Use string multiplication to create a string of stars for this row (row stars).
+   4. Print the spaces and stars for this row.
+5. Print a
+*/
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
